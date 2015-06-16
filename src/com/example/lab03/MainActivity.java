dad1f7a2d6014d8802eaf788aefbc91f2cd32a2c
eduardolam <eduardolam@gmail.com>
2015-06-15 21:26:58 -0300

    Lab03

--- src/com/example/lab03/MainActivity.java
+++ src/com/example/lab03/MainActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+/* 
+ * 
+ * @author Eduardo Luiz Arthur Marques - 12296045
+ * 
+ *
+ */
+
 public class MainActivity extends ActionBarActivity {
 
 	@Override
